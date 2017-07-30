96b9fa3f67c0e4681f79e57ed1861396dbe9032a
Andrew <andrewsouthard111@gmail.com>
2017-07-30 12:14:11 -0400

    added dynamic programming questions

--- arrayAndString.js
+++ arrayAndString.js
@@ -10,117 +10,49 @@ function isUnique(str){
   return true;
 }
 
-// The nth fibonacci number is defined in terms of the two previous fibonacci numbers, so this seems to lend itself to recursion
-
-// function fib(n){
-//   if(n === 0 || n === 1){
-//     return n;
-//   } else {
-//     return fib(n-2) + fib(n-1);
-//   }
-// }
-
-function Fibber(){
-  this.memo = {}
-}
-
-Fibber.prototype.fib = function(n){
-  
-  if(n < 0) {
-    throw new Error('Index was negative');
-  } else if (n === 0 || n === 1){
+function recursiveFib(n){
+  if(n === 1 || n === 0){
     return n;
   }
-  if (this.memo.hasOwnProperty(n)){
-    console.log('grabbing memo[' + n + ']');
-    return this.memo[n];
-  } 
-  console.log('computing fib(' + n +')');
-  var result = (this.fib(n - 2) + this.fib(n - 1));
-  this.memo[n] = result;
-  console.log('this.memo' + '[' + n + ']' + this.memo[n]);
-  return result;
+  return recursiveFib(n - 2) + recursiveFib(n - 1);
 }
 
-function CoinRegister() {
+function Fibber(){
   this.memo = {};
 }
 
-CoinRegister.prototype.changePossibilitiesTopDown = function(amountLeft, denominations, currentIndex) {
-
-  currentIndex = (typeof currentIndex !== 'undefined' ? currentIndex : 0);
-
-  // base cases:
-  // we hit the amount spot on. yes!
-  if (amountLeft === 0){
-    return 1; 
-  }
-
-  // we overshot the amount left (used too many coins)
-  if (amountLeft < 0){
-    return 0;
-  }
-
-  // we're out of denominations
-  if (currentIndex >= denominations.length){
-    return 0;
-  }
-
-  if (this.memo.hasOwnProperty(amountLeft)){
-    return this.memo[amountLeft];
-  } else {
-
-  // choose a current coin
-    var currentCoin = denominations[currentIndex];
-
-    // see how many possibilities we can get
-    // for each number of times to use currentCoin
-    var numPossibilites = 0;
-    while(amountLeft >= 0){
-      numPossibilites += changePossibilitiesTopDown(amountLeft, denominations, currentIndex + 1);
-      amountLeft -= currentCoin;
-    }
-  this.memo[amountLeft] = numPossibilites;
-  return numPossibilites;
-}
-}
-
-
-function factorial(n){
-  if(n === 1){
+Fibber.prototype.fib = function(n){
+  if(n === 1 || n === 0){
     return n;
-  } else {
-    console.log("computing factorial " + n);
-    return n * factorial(n-1);
+  } 
+  if (this.memo.hasOwnProperty(n)){
+    console.log('grabbing memo[' + n + ']');
+    return this.memo[n];
   }
+  console.log('computing fib(' + n +')');
+  this.memo[n] = this.fib(n-2) + this.fib(n-1);
+  return this.fib(n-2) + this.fib(n-1);
 }
 
-function sumNats(n){
-  if(n === 1){
+function bottomUpFib(n){
+  if(n < 0) {
+    throw new Error ("can't be negative");
+  } else if (n === 1 || n === 0){
     return n;
-  } else {
-    console.log("computing sumNats " + n);
-    return n + sumNats(n-1);
   }
+
+  var twoBehind = 0;
+  var oneBehind = 1;
+  var fib = 0;
+
+  for(var i = 1; i < n; i++){
+    fib = twoBehind + oneBehind;
+    twoBehind = oneBehind;
+    oneBehind = fib;
+  }
+  return fib;
 }
 
-function multiply(a, b){
-  if(b === 1){
-    return a;
-  } else {
-    console.log("computing multiply " + a + b);
-    return a + multiply(a, b-1);
-  }
-}
-
-function power(a, b){
-  if(b === 1){
-    return a;
-  } else {
-    console.log("computing power " + a + b);
-    return a * power(a, b - 1);
-  }
-}
 
 
 
